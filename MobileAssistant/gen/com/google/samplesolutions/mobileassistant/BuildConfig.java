4aee0368d37aee291cc2d02427e5cf19e8b5bbb5
jli486 <jli@gatech.edu>
2014-03-24 22:34:49 -0700

    initial template

---
+++ MobileAssistant/gen/com/google/samplesolutions/mobileassistant/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.google.samplesolutions.mobileassistant;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
